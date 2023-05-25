999dda2dca9eb9973feaaa56b0ca5fe1a1e416dc
Hollin A <ariyawansahollin@gmail.com>
2023-05-26 01:46:55 +0530

    made changes to homepa ge

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,43 @@
 import classes from "./Navbar.module.css";
 
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
+// import NavbarShape from "../assets/navbar-shape.png";
 
 const Navbar = () => {
   return (
     <div className={classes.navbar}>
+      {/* <svg
+        width="1440"
+        height="162"
+        viewBox="0 0 1440 162"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className={classes.svg}
+      >
+        <path
+          d="M-2 -7H1467V31.6343C1467 85.9994 1423.57 130.405 1369.22 131.61L-2 162V-7Z"
+          fill="url(#paint0_linear_1_5)"
+          fill-opacity="0.87"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_1_5"
+            x1="732.5"
+            y1="-7"
+            x2="732.5"
+            y2="162"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#736E6E" />
+            <stop offset="1" stop-color="#4B4141" />
+          </linearGradient>
+        </defs>
+      </svg> */}
+      {/* <img
+        arc={NavbarShape}
+        alt="navbar polygon"
+        className={classes.navbarShape}
+      /> */}
       <img src={logo} alt="logo" className={classes.logo} />
     </div>
   );
--- frontend/src/components/PreviousEditionsCarousel.js
+++ frontend/src/components/PreviousEditionsCarousel.js
@@ -1,5 +1,54 @@
+import React, { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import "./PreviousEditionsCarousel.css";
+
 const PreviousEditionsCarousel = () => {
-  return <div>PreviousEditionsCarousel</div>;
+  const [sliderRef, setSliderRef] = useState(null);
+
+  const settings = {
+    arrows: false,
+    dots: true,
+    infinite: true,
+    // speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="carouselContainer container">
+      <div className="controls">
+        <span
+          class="material-symbols-outlined prevIcon"
+          onClick={sliderRef?.slickPrev}
+        >
+          arrow_back_ios
+        </span>
+        <span
+          class="material-symbols-outlined nextIcon"
+          onClick={sliderRef?.slickNext}
+        >
+          arrow_forward_ios
+        </span>
+      </div>
+      <Slider ref={setSliderRef} {...settings}>
+        <div className="previousEditionDiv">
+          <div className="previousEdition">Editon 01</div>
+        </div>
+        <div className="previousEditionDiv">
+          <div className="previousEdition">Editon 02</div>
+        </div>
+        <div className="previousEditionDiv">
+          <div className="previousEdition">Editon 03</div>
+        </div>
+        <div className="previousEditionDiv">
+          <div className="previousEdition">Editon 04</div>
+        </div>
+      </Slider>
+    </div>
+  );
 };
 
 export default PreviousEditionsCarousel;
