6dcefbd368e17f3b5ee74075d825636312d64e25
Hollin A <ariyawansahollin@gmail.com>
2023-05-26 04:04:05 +0530

    added the newspaperview

--- frontend/src/components/NewspaperView.js
+++ frontend/src/components/NewspaperView.js
@@ -1,10 +1,19 @@
+import HTMLFlipBook from "react-pageflip";
+
 import classes from "./NewspaperView.module.css";
 
 const NewspaperView = () => {
   return (
     <div>
       <p className={classes.tagline}>Read the latest Rathnadeepa Online</p>
-      <div className={classes.viewSection}>View Area</div>
+      <div className={classes.viewSection}>
+        <HTMLFlipBook width={300} height={400}>
+          <div className={classes.demoPage}>Page 1</div>
+          <div className={classes.demoPage}>Page 2</div>
+          <div className={classes.demoPage}>Page 3</div>
+          <div className={classes.demoPage}>Page 4</div>
+        </HTMLFlipBook>
+      </div>
     </div>
   );
 };
