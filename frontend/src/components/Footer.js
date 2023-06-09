664e896dc332d5264e997ebae691162cd4ce1f5c
Hollin A <ariyawansahollin@gmail.com>
2023-06-10 03:10:07 +0530

    updated the footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,23 +1,14 @@
 import classes from "./Footer.module.css";
 
 import Logo from "../assets/logo.png";
+import Facebook from "../assets/facebook.png";
+import Youtube from "../assets/youtube.png";
 
 const Footer = () => {
   return (
     <div className={classes.footer}>
       <div className="container py-5">
-        <div className="row">
-          <div className="col-lg-9 col-12">
-            <p className={classes.wannaKnowText}>Wanna know more about us ?</p>
-            <p className={classes.contactUsText}>Contact us for more details</p>
-          </div>
-          <div className={`col-lg-3 col-12 ${classes.footerTopRight}`}>
-            <button className={classes.btn}>
-              <p className={classes.btnText}>Contact Us</p>
-            </button>
-          </div>
-        </div>
-        <div className={`${classes.bottomRow} row mt-3 gx-5`}>
+        <div className="row g-0">
           <div className="col-lg-6 col-12">
             <img src={Logo} className={classes.logo} alt="logo" />
             <p className={classes.tagline}>
@@ -25,19 +16,34 @@ const Footer = () => {
               facilisis, leo sit amet suscipit aliquet, tellus tortor tristique
               purus, sit amet
             </p>
+            <button className={classes.btn}>
+              <p className={classes.btnText}>Contact Us for more Details</p>
+            </button>
+            <div className={classes.socialMediaDiv}>
+              <img
+                src={Facebook}
+                className={classes.socialMedia}
+                alt="facebook"
+              />
+              <img
+                src={Youtube}
+                className={classes.socialMedia}
+                alt="youtube"
+              />
+            </div>
           </div>
-          <div className="col-lg-3 col-6">
-            <p className={classes.contactUsTitle}>Contact Us</p>
+          <div className="offset-lg-2 col-lg-4 col-12 mt-l-0 mt-4">
             <p className={classes.callUsOn}>Call Us On</p>
             <p className={classes.phoneNumbers}>Sahan +61 449 776 750</p>
             <p className={classes.phoneNumbers}>Ann +61 452 670 045</p>
+            <p className={classes.addressTitle}>Address</p>
+            <p className={classes.address}>
+              2/1265 Nepean Highway,
+              <br />
+              Cheltenham Vic 3192
+            </p>
             <p className={classes.emailTitle}>Email</p>
             <p className={classes.email}>ads@rathnadeepa.au</p>
-          </div>
-          <div className="col-lg-3 col-6">
-            <p className={classes.getInTouch}>Get in Touch</p>
-            <p className={classes.socialMedia}>Twitter</p>
-            <p className={classes.socialMedia}>Facebook</p>
           </div>
         </div>
       </div>
